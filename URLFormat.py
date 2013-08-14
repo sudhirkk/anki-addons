55bc0f3bcd301f55df66278770f7d3f990b0b939
Sudhir Krishnan <sudhirkk@gmail.com>
2013-08-13 23:43:16 -0400

    Create URLFormat.py

---
+++ URLFormat.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+# Copyright 2013 Sudhir Krishnan <http://www.sudhirwarrier.com>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from anki.hooks import wrap
+from aqt.editor import Editor
+from anki.utils import json
+
+def urlFormat(self):
+        the_url = "".join(["<a href=\"",
+                               self.web.selectedText(),
+                               "\">", self.web.selectedText(), "</a>"])
+        self.web.eval("document.execCommand('inserthtml', false, %s);"
+                  % json.dumps(the_url))
+
+def setupButtons(self):
+    self._addButton("urlButton", lambda s=self: urlFormat(self),
+                    text=u"L", tip="HTML (Ctrl+Shift+h", key="Ctrl+Shift+h")
+
+Editor.urlFormat = urlFormat
+Editor.setupButtons = wrap(Editor.setupButtons, setupButtons)
+
